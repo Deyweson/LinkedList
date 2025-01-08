295a2a99475e917ea56b61092e0233f016d5d7a8
Deyweson <deywesonalm@gmail.com>
2025-01-08 17:01:20 -0300

    :sparkles: Feat: Linked List

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,4 +1,65 @@
-package PACKAGE_NAME;
-
 public class LinkedList {
+    Node head;
+
+    LinkedList(){
+        this.head = null;
+    }
+
+    public void AppendNode(Node newNode){
+        Node current = this.head;
+        if(current == null){
+            this.head = newNode;
+        }else {
+            // Anda até o final da lista
+            while(current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void InsertNode(Node newNode, int index){
+        Node current = this.head;
+
+        for(int i =0; i < index -1 && current != null; i++){
+            current = current.next;
+        }
+
+        if(current != null){
+            newNode.next = current.next;
+            current.next = newNode;
+        }
+    }
+
+    public int DeleteNode(int index){
+
+        Node current = this.head;
+        Node prev = null;
+        int deletedValue = -1;
+
+        if(index == 0){
+            deletedValue = current.data;
+            this.head = this.head.next;
+            return deletedValue;
+        }
+
+        for (int i = 0; i < index && current != null; i++) {
+            prev = current;
+            current = current.next;
+        }
+        if(current != null){
+            deletedValue = current.data;
+            prev.next = current.next;
+        }
+        return deletedValue;
+    }
+
+    public void DisplayList(){
+        Node current = this.head;
+        while(current != null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        LinkedList list = new LinkedList();
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        Node node3 = new Node(3);
+        Node node4 = new Node(4);
+        Node node5 = new Node(5);
+
+        list.AppendNode(node1);
+        list.AppendNode(node2);
+        list.AppendNode(node3);
+        list.AppendNode(node4);
+        list.AppendNode(node5);
+        list.DisplayList();
+
+        list.DeleteNode(3);
+        list.DisplayList();
+
+        list.InsertNode(node4, 3);
+        list.DisplayList();
     }
 }
+
+
+
